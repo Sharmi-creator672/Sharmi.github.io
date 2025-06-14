cfc5c8703b04dbb7848155a1522d2d94abe13c54
Sharmi-creator672 <sharmelym.delgado@ucsp.edu.pe>
2025-06-14 12:14:54 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ const env = loadEnv("", process.cwd(), ["STORYBLOK", "NETLIFY"]);
  
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astrogency.unfolding.io/", // change this to your domain
+  site: "https://sharmi.github.io/", // change this to your domain
 
   adapter: env.NETLIFY ? netlify() : vercel(), // vercel() or netlify()
   integrations: [
